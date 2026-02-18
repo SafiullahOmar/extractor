940b1badbc71f2c1be05ecf3071fba5423ebccf6
safiullah omar <omar.safiullah@gmail.com>
2026-02-18 20:22:36 +0100

    testing

--- curation_agents.py
+++ curation_agents.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 llm = ChatOpenAI(
-    model=os.getenv('gpt-4o-mini'),
+    model=os.getenv('OPENAI_MODEL', 'gpt-4o-mini'),
     temperature=0,
     api_key=os.getenv('OPENAI_API_KEY')
 )
